b46ac687d3e35328709dc3faf47e02e3a4e00840
Lsteenhuis <larssteenhuis@gmail.com>
2017-02-22 12:50:19 +0100

    deprecated, geneLevelExtremeMatrices is the correct file.

--- code/r/sortCellAbundance.R
+++ code/r/sortCellAbundance.R
@@ -56,38 +56,52 @@ colnames(highValues) <- columnNames
 
 # This function creates two data frames for the gene level expression data 
 # intended for the gene level expression data. It sorts the data per row ( per gene)
-# and takes the top 25% and bottom 25% expressed samples
+# and takes the top 25% and bottom 25% expressed samples.
+# data frame: Two columns per sample: first column contains expression data,
+# second column contains corresponding sample name.
 
+#initialisation of the data frames.
 highValuesGen = NULL
 lowValuesGen = NULL
-createExpressionMatrix <- function(x){
-  orderedRow <- order(x, decreasing = T)
-  length <- length(x) / 100 * 25
-    
-  newHighValues <- x[orderedRow][1:length]
-  samplesHigh = colnames(geneLevelExpression)[orderedRow[1:length]]
+# the percentage that will be taken from the gene row
+percentage <- length(geneLevelExpression[1,]) / 100 * 25
 
-  newLowValues <- x[rev(orderedRow)][1:length]
-  samplesLow = colnames(geneLevelExpression)[rev(orderedRow)[1:length]]
-
+createExpressionMatrix <- function(geneRow){
+  # ordering of gene row high -> low
+  orderedGeneRow <- order(geneRow, decreasing = T)
+  
+  # gets top expression values from the orderered gene row
+  newHighValues <- geneRow[orderedGeneRow][1:percentage]
+  # retrieves sample names for the top expressed samples
+  samplesHigh = colnames(geneLevelExpression)[orderedGeneRow[1:percentage]]
+  
+  # gets bottom expression values from the ordered gene row
+  newLowValues <- geneRow[rev(orderedGeneRow)][1:percentage]
+  # retrieves the sample names for the bottom expressed samples
+  samplesLow = colnames(geneLevelExpression)[rev(orderedGeneRow)[1:percentage]]
+  
+  # adds the two lists ( of high and bottom expressed values ) and adds them to the data frame as column
   highValuesGen <<- cbind(highValuesGen,newHighValues,samplesHigh)
   lowValuesGen <<- cbind(lowValuesGen, newLowValues,samplesLow)
 }
 
 apply(geneLevelExpression,1, createExpressionMatrix)
 
+# Small function which retrieves the ENSEMBL id's from the geneLevelExpression data set.
+# Creates column names for the high and low epxressed data frames.
+# Per ENSEMBL ID also create a row with ID-samples. 
 ensemblList <- rownames(geneLevelExpression)
 columnNames = NULL
 createColNameMatrix <- function(ensId){
   columnNames <<- cbind(columnNames,ensId, paste(ensId,"-samples",sep=""))
 }
-
 lapply(ensemblList, createColNameMatrix)
 
+# resets row names of data frames and set column names.
 rownames(highValuesGen) <- NULL
 rownames(lowValuesGen) <- NULL
 colnames(highValuesGen) <- columnNames
-colnames(lowValuesGen) <- columnNamesLow
+colnames(lowValuesGen) <- columnNames
 
 
 
